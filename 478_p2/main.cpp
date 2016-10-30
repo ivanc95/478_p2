5c9a0c0e858d220afa8017588938c464a09f12d0
ivanc95 <ivanc95@email.arizona.edu>
2016-10-30 13:06:34 -0700

    Initial Commit

---
+++ 478_p2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  478_p2
+//
+//  Created by Ivan Cordoba on 10/30/16.
+//  Copyright © 2016 Ivan Cordoba. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
